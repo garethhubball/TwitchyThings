675cd0e89bccaa22dd2ffef758f2333effb61cb2
Gareth Hubball <ridecar2@gmail.com>
2022-02-09 02:06:02 +0000

    Initial commit

---
+++ AuthServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace TwitchyThings;
+
+public static class AuthServer
+{
+    public static Task RunServer()
+    {
+        var builder = WebApplication.CreateBuilder();
+        var app = builder.Build();
+
+        app.MapGet("/", () =>
+        {
+            return "Hello world!";
+        });
+
+        return app.RunAsync("https://localhost:3000");
+    }
+}
